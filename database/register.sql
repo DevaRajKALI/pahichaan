ab806c0b89b0282fb73a9333df52c514322ad841
thearjun <mailarjunadhikari@gmail.com>
2019-09-20 12:29:04 +0545

    Database for Registring User

---
+++ database/register.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE `social`.`posts`
+  (
+     `id`          INT NOT NULL auto_increment,
+     `body`        TEXT NOT NULL,
+     `added_by`    VARCHAR(100) NOT NULL,
+     `user_to`     VARCHAR(100) NOT NULL,
+     `date_added`  DATETIME NOT NULL,
+     `user_closed` VARCHAR(3) NOT NULL,
+     `deleted`     VARCHAR(3) NOT NULL,
+     `likes`       INT NOT NULL,
+     PRIMARY KEY (`id`)
+  )
+engine = innodb;
