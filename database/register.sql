26ab55b3fc12825a1b24103692fcd90e6f8fe4ae
thearjun <mailarjunadhikari@gmail.com>
2019-09-20 13:33:53 +0545

    Corrected Code for Registring User

--- database/register.sql
+++ database/register.sql
@@ -1,14 +1,15 @@
-
-CREATE TABLE `pahichaan`.`posts`
-  (
-     `id`          INT NOT NULL auto_increment,
-     `body`        TEXT NOT NULL,
-     `added_by`    VARCHAR(100) NOT NULL,
-     `user_to`     VARCHAR(100) NOT NULL,
-     `date_added`  DATETIME NOT NULL,
-     `user_closed` VARCHAR(3) NOT NULL,
-     `deleted`     VARCHAR(3) NOT NULL,
-     `likes`       INT NOT NULL,
-     PRIMARY KEY (`id`)
-  )
-engine = innodb;
+CREATE TABLE `social`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(25) NOT NULL,
+    `last_name` VARCHAR(25) NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `signup_date` DATE NOT NULL,
+    `profile_pic` VARCHAR(255) NOT NULL,
+    `num_posts` INT NOT NULL,
+    `num_likes` INT NOT NULL,
+    `users_closed` VARCHAR(3) NOT NULL,
+    `friend_array` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
+)  ENGINE=INNODB;
